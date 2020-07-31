9d972609a39d28905fe95b570b0e375f7574d000
Murat karabulut <mkarabulut44@hotmail.com>
2020-07-31 23:42:56 +0300

    Delete KeyValuePairModelDescription.cs

--- frontend/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace Web.ChatBot.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
