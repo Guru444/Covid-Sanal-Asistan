39a4c487e7f541ec4ff26082563a7a1cb7861d37
Murat karabulut <mkarabulut44@hotmail.com>
2020-07-31 23:42:05 +0300

    Delete CollectionModelDescription.cs

--- frontend/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Web.ChatBot.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
